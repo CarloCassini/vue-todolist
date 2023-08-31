e0c463890be39b68be6757b654f85b6b310389da
CarloCassini <c.cassini87@gmail.com>
2023-08-31 14:41:20 +0200

    initialize

---
+++ js/main.js
@@ -0,0 +1,36 @@
+console.log("main ok");
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      todo: [
+        {
+          text: "milestone 1",
+          todo: false,
+        },
+        {
+          text: "milestone 2",
+          todo: false,
+        },
+        {
+          text: "milestone 3",
+          todo: false,
+        },
+        {
+          text: "bonus 1",
+          todo: false,
+        },
+        {
+          text: "bonus 2",
+          todo: false,
+        },
+      ],
+    };
+  },
+
+  methods: {},
+
+  created() {},
+}).mount("#app");
